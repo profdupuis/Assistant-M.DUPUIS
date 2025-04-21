009ecde8875ca16c3164f20742761d8c4562eb1a
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-21 17:03:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -159,5 +159,7 @@ def reset():
     return redirect(url_for("login"))
 
 # ─────────── run ───────────────────────────────────────────────────
+# Lancement de l'application Flask en mode debug
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
