d6182988822dcf82c1439f2234ac6ec6cbe4eeba
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-24 13:38:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1085,4 +1085,5 @@ def dashboard_exercice(exo):
 
 # ─────────── run ───────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
