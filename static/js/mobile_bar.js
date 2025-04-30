2a9478083c16bf697770356e74c49fa33e462e9b
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-30 21:50:18 +0200

    Delete static directory

--- static/js/mobile_bar.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.matiere-select').addEventListener('change', e => {
-  const map={NSI : '💻',MATHS:'📘',SVT:'🧬','':'📚'};
-  document.getElementById('matiere-emoji').textContent = map[e.target.value]||'📚';
-});
