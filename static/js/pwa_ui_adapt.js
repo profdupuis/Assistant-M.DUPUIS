9f4549ea2b01bcf6683fed392d616daac2f828d6
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-05-10 21:36:27 +0200

    Delete static directory

--- static/js/pwa_ui_adapt.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches
-                    || window.navigator.standalone === true;
-
-  if (isStandalone) {
-    document.body.classList.add('pwa-installed');
-  }
-});
