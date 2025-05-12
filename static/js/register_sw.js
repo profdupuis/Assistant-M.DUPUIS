5457c031c4ef4ab6e3da38b0a919e15c8776cee7
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-05-12 21:16:13 +0200

    Delete static/js directory

--- static/js/register_sw.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/static/service-worker.js')
-    .then(reg => console.log("✅ Service worker enregistré"))
-    .catch(err => console.warn("❌ Service worker erreur", err));
-}
